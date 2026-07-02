e8167c86ca15b1b162a401d92c049ca63caf0fd2
Zhihao Ouyang <2743400561@qq.com>
2026-07-02 22:44:20 +0800

    11

--- miniprogram/utils/config.js
+++ miniprogram/utils/config.js
@@ -8,6 +8,6 @@
  */
 
 // ⚠️ 部署到 CloudBase 后，把下面的 localhost 改成你的公网域名
-const BASE_URL = 'https://sleep-care-d2gq3ydpc800cdc9-1449652234.ap-shanghai.app.tcloudbase.com';
+const BASE_URL = 'https://sleepcare-api-277257-9-1449652234.sh.run.tcloudbase.com';
 
 module.exports = { BASE_URL };
